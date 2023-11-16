34396bc43e1a8541ce69e3f85dda8f4b34f47a37
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2023-11-16 20:00:35 +0700

    search

--- BE_tasteal/Persistence/Repository/RecipeRepo/RecipeSearchRepo.cs
+++ BE_tasteal/Persistence/Repository/RecipeRepo/RecipeSearchRepo.cs
@@ -3,6 +3,9 @@ using BE_tasteal.Entity.Entity;
 using BE_tasteal.Persistence.Context;
 using BE_tasteal.Persistence.Repository.GenericRepository;
 using Dapper;
+using System.Data.Common;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace BE_tasteal.Persistence.Repository.RecipeRepo
 {
@@ -13,47 +16,66 @@ namespace BE_tasteal.Persistence.Repository.RecipeRepo
         {
 
         }
-        public async Task<List<RecipeEntity>> Search(RecipeSearchReq data)
+        public async Task<List<RecipeEntity>> Search(RecipeSearchReq input)
         {
             using (var connection = _connection.GetConnection())
             {
-
-                string query = @"
-                SELECT DISTINCT *
-                FROM recipe AS r
-                INNER JOIN Nutrition_Info AS n_i ON r.nutrition_info_id = n_i.id
-                INNER JOIN Ingredient AS i ON n_i.id = i.nutrition_info_id
-                INNER JOIN Account AS a ON a.id = r.author
-                INNER JOIN Recipe_Occasion AS r_o ON r.id= r_o.recipe_id
-                WHERE 
-                (@IngredientID IS NULL OR i.id IN @IngredientID) AND
-                (@ExceptIngredientID IS NULL OR i.id NOT IN @ExceptIngredientID) AND
-                (@TotalTime IS NULL OR r.totalTime <= @TotalTime) AND
-                (@ActiveTime IS NULL OR r.active_time <= @ActiveTime) AND
-                (@OccasionID IS NULL OR r_o.occasion_id IN @OccasionID) AND           
-		        (@KeyWords IS NULL OR REGEXP_LIKE(r.introduction, @KeyWordsFormat)) AND
-                ((@TextSearch IS NULL) OR
-                (r.name LIKE '%' + @TextSearch + '%' OR
-                r.introduction LIKE '%' + @TextSearch + '%' OR
-                a.username LIKE '%' + @TextSearch + '%' OR 
-                i.name LIKE '%' + @TextSearch + '%'))";
-                //(@Calories IS NULL OR n_i.calories <= @Calories) AND
-                //(@Calories IS NULL OR n_i.calories <= @Calories) AND
-                var result = connection.Query<RecipeEntity>(query, new
+                string sql = @"SELECT r.*
+                   FROM Recipe r
+                        JOIN Recipe_Ingredient ri ON r.id = ri.recipe_id
+                        JOIN Ingredient i ON ri.ingredient_id = i.id
+                        LEFT JOIN Recipe_Occasion ro ON r.id = ro.recipe_id
+                   WHERE (@IngredientID IS NULL OR i.id IN @IngredientID)
+                     AND (@ExceptIngredientID IS NULL OR i.id NOT IN @ExceptIngredientID)
+                    
+                   GROUP BY r.id"
+                ;
+                var result = await connection.QueryAsync<RecipeEntity>(sql, new
                 {
-                    IngredientID = data.IngredientID,
-                    ExceptIngredientID = data.ExceptIngredientID,
-                    TotalTime = data.TotalTime,
-                    ActiveTime = data.ActiveTime,
-                    OccasionID = data.OccasionID,
-                    TextSearch = data.TextSearch,
-                    KeyWords = data.KeyWords,
-                    KeyWordsFormat = data.KeyWordsFormat,
+                    IngredientID = input.IngredientID,
+                    ExceptIngredientID = input.ExceptIngredientID,
+                    MinCalories = input.Calories?.min,
+                    MaxCalories = input.Calories?.max
                 });
-
                 return result.ToList();
             }
 
         }
     }
 }
+
+//search Binh
+//string query = @"
+//                SELECT DISTINCT *
+//                FROM recipe AS r
+//                INNER JOIN Nutrition_Info AS n_i ON r.nutrition_info_id = n_i.id
+//                INNER JOIN Ingredient AS i ON n_i.id = i.nutrition_info_id
+//                INNER JOIN Account AS a ON a.id = r.author
+//                INNER JOIN Recipe_Occasion AS r_o ON r.id= r_o.recipe_id
+//                WHERE 
+//                (@IngredientID IS NULL OR i.id IN @IngredientID) AND
+//                (@ExceptIngredientID IS NULL OR i.id NOT IN @ExceptIngredientID) AND
+//                (@TotalTime IS NULL OR r.totalTime <= @TotalTime) AND
+//                (@ActiveTime IS NULL OR r.active_time <= @ActiveTime) AND
+//                (@OccasionID IS NULL OR r_o.occasion_id IN @OccasionID) AND           
+//		        (@KeyWords IS NULL OR REGEXP_LIKE(r.introduction, @KeyWordsFormat)) AND
+//                ((@TextSearch IS NULL) OR
+//                (r.name LIKE '%' + @TextSearch + '%' OR
+//                r.introduction LIKE '%' + @TextSearch + '%' OR
+//                a.username LIKE '%' + @TextSearch + '%' OR 
+//                i.name LIKE '%' + @TextSearch + '%'))";
+//(@Calories IS NULL OR n_i.calories <= @Calories) AND
+//(@Calories IS NULL OR n_i.calories <= @Calories) AND
+//var result = connection.Query<RecipeEntity>(query, new
+//{
+//    IngredientID = data.IngredientID,
+//    ExceptIngredientID = data.ExceptIngredientID,
+//    TotalTime = data.TotalTime,
+//    ActiveTime = data.ActiveTime,
+//    OccasionID = data.OccasionID,
+//    TextSearch = data.TextSearch,
+//    KeyWords = data.KeyWords,
+//    KeyWordsFormat = data.KeyWordsFormat,
+//});
+
+//return result.ToList();
