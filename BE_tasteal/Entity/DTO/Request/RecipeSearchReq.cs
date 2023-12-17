ded47863cc15a3936dd07926eba4059452b09692
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2023-12-17 18:21:07 +0700

    recipe search

--- BE_tasteal/Entity/DTO/Request/RecipeSearchReq.cs
+++ BE_tasteal/Entity/DTO/Request/RecipeSearchReq.cs
@@ -5,8 +5,10 @@ namespace BE_tasteal.Entity.DTO.Request
     public class RecipeSearchReq
     {
         [Range(1, int.MaxValue)]
+        [Required]
         public int? page { get; set; }
         [Range(1, int.MaxValue)]
+        [Required]
         public int? pageSize { get; set; }
         public IEnumerable<int>? IngredientID { get; set; }
         public IEnumerable<int>? ExceptIngredientID { get; set; }
