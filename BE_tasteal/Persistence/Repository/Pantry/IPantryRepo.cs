711c3ccc0088a642fd6fdf836bd969907eaf81ee
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2024-01-01 12:12:21 +0700

    pantry ingredient recommend

---
+++ BE_tasteal/Persistence/Repository/Pantry/IPantryRepo.cs
@@ -0,0 +1,14 @@
+﻿using BE_tasteal.Entity.DTO.Request;
+using BE_tasteal.Entity.Entity;
+using BE_tasteal.Persistence.Repository.GenericRepository;
+
+namespace BE_tasteal.Persistence.Repository.Pantry
+{
+    public interface IPantryRepo : IGenericRepository<PantryEntity>
+    {
+        List<RecipeEntity> FindGroupIndexContainingAnyValue(RecipesIngreAny req);
+        List<RecipeEntity> FindGroupIndexContainingAllValues(RecipesIngreAny req);
+        List<RecipeEntity> FindGroupIndexContainingAnyValuesPantry(RecipesPantryAny req);
+        List<RecipeEntity> FindGroupIndexContainingAllValuesPantry(RecipesPantryAny req);
+    }
+}
