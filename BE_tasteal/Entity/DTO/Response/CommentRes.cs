6cc6fc6c9a190bf315a6a0600559e1677b8bc552
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2024-01-02 22:26:33 +0700

    comment

--- BE_tasteal/Entity/DTO/Response/CommentRes.cs
+++ BE_tasteal/Entity/DTO/Response/CommentRes.cs
@@ -3,8 +3,11 @@
     public class CommentRes
     {
         public int id { get; set; }
-        public string account_id {  get; set; }
+        public string account_id { get; set; }
         public string name { get; set; }
         public string comment { get; set; }
+        public string? image { get; set; }
+        public string created_at { get; set; }
+        public string updated_at { get; set; }
     }
 }
--- BE_tasteal/Entity/DTO/Response/IngredientRes.cs
+++ BE_tasteal/Entity/DTO/Response/IngredientRes.cs
@@ -2,7 +2,7 @@
 {
     public class IngredientRes
     {
-        public string Id { get; set; }
+        public string id { get; set; }
         public string name { get; set; }
         public string? image { get; set; }
         public decimal? amount { get; set; }
