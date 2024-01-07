64b5df0b8b0b7eb527cd8a3fadb5b646b90d367b
TriTran951 <90551445+TriTran951@users.noreply.github.com>
2024-01-07 12:21:12 +0700

    ingredient crud

--- BE_tasteal/API/Controllers/IngredientController.cs
+++ BE_tasteal/API/Controllers/IngredientController.cs
@@ -2,6 +2,8 @@
 using BE_tasteal.Entity.DTO.Request;
 using BE_tasteal.Entity.Entity;
 using BE_tasteal.Persistence.Repository.IngredientRepo;
+using BE_tasteal.Persistence.Repository.IngredientTypeRepo;
+using BE_tasteal.Persistence.Repository.NutritionRepo;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BE_tasteal.API.Controllers
@@ -13,13 +15,18 @@ namespace BE_tasteal.API.Controllers
     {
         private readonly IIngredientBusiness _ingredientBusiness;
         private readonly IIngredientRepo _ingredientRepo;
-
+        private readonly INutritionRepo _nutritionRepo;
+        private readonly IIngredientTypeRepo _ingredientTypeRepo;
         public IngredientController(
            IIngredientBusiness ingredientBusiness,
-           IIngredientRepo ingredientRepo)
+           IIngredientRepo ingredientRepo,
+           INutritionRepo nutritionRepo,
+           IIngredientTypeRepo ingredientTypeRepo)
         {
             _ingredientBusiness = ingredientBusiness;
             _ingredientRepo = ingredientRepo;
+            _nutritionRepo = nutritionRepo;
+            _ingredientTypeRepo = ingredientTypeRepo;
         }
 
         [HttpPost]
@@ -95,6 +102,134 @@ namespace BE_tasteal.API.Controllers
             {
                 Console.WriteLine(ex.ToString());
                 return BadRequest(false);
+            }
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> get(int id)
+        {
+            try
+            {
+                var result = await _ingredientRepo.FindById(id);
+
+                if (result == null)
+                    return BadRequest("Id invalid");
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return BadRequest(false);
+            }
+        }
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> postIngredient([FromBody] IngredientPutReq req)
+        {
+            try
+            {
+                var type = await _ingredientTypeRepo.FindByIdAsync(req.ingredient_type.id);
+                if (type == null)
+                    return BadRequest("TypeId invalid");
+
+                Nutrition_InfoEntity nutri_info = new Nutrition_InfoEntity
+                {
+                    calories = req.nutrition_info.calories,
+                    fat = req.nutrition_info.fat,
+                    saturated_fat = req.nutrition_info.saturated_fat,
+                    trans_fat = req.nutrition_info.trans_fat,
+                    cholesterol = req.nutrition_info.cholesterol,
+                    carbohydrates = req.nutrition_info.carbohydrates,
+                    fiber = req.nutrition_info.fiber,
+                    sugars = req.nutrition_info.sugars,
+                    protein = req.nutrition_info.protein,
+                    sodium = req.nutrition_info.sodium,
+                    vitaminD = req.nutrition_info.vitaminD,
+                    calcium = req.nutrition_info.calcium,
+                    iron = req.nutrition_info.iron,
+                    potassium = req.nutrition_info.potassium
+                };
+
+                var new_nutr_info = await _nutritionRepo.InsertAsync(nutri_info);
+
+                if (new_nutr_info == null)
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+
+                IngredientEntity ingredient = new IngredientEntity
+                {
+                    name = req.name,
+                    image = req.image,
+                    nutrition_info_id = new_nutr_info.id,
+                    type_id = type.id,
+                    isLiquid = req.isLiquid,
+                    ratio = req.ratio
+                };
+
+                var new_ingredient = await _ingredientRepo.InsertAsync(ingredient);
+
+                if (new_ingredient == null)
+                    return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+
+                return Ok(new_ingredient);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
+            }
+        }
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> putIngredient(int id, [FromBody] IngredientPutReq req)
+        {
+            try
+            {
+                var ingredient = await _ingredientRepo.FindByIdAsync(id);
+
+                if (ingredient == null)
+                    return BadRequest("IngredientId invalid");
+
+
+                {
+                    var nutri_info = await _nutritionRepo.FindByIdAsync(ingredient.nutrition_info_id);
+                    nutri_info.calories = req.nutrition_info.calories;
+                    nutri_info.fat = req.nutrition_info.fat;
+                    nutri_info.saturated_fat = req.nutrition_info.saturated_fat;
+                    nutri_info.trans_fat = req.nutrition_info.trans_fat;
+                    nutri_info.cholesterol = req.nutrition_info.cholesterol;
+                    nutri_info.carbohydrates = req.nutrition_info.carbohydrates;
+                    nutri_info.fiber = req.nutrition_info.fiber;
+                    nutri_info.sugars = req.nutrition_info.sugars;
+                    nutri_info.protein = req.nutrition_info.protein;
+                    nutri_info.sodium = req.nutrition_info.sodium;
+                    nutri_info.vitaminD = req.nutrition_info.vitaminD;
+                    nutri_info.calcium = req.nutrition_info.calcium;
+                    nutri_info.iron = req.nutrition_info.iron;
+                    nutri_info.potassium = req.nutrition_info.potassium;
+
+                    await _nutritionRepo.UpdateAsync(nutri_info);
+                }
+
+                {
+                    ingredient.name = req.name;
+                    ingredient.type_id = req.ingredient_type.id;
+                    ingredient.image = req.image;
+                    ingredient.isLiquid = req.isLiquid;
+                    ingredient.ratio = req.ratio;
+                }
+
+                await _ingredientRepo.UpdateAsync(ingredient);
+
+
+
+                return Ok(ingredient);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal Server Error");
             }
         }
     }
